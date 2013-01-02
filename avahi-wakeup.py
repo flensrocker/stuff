c3c18f46f3f50ed2117bc4d4a46b51e040d9d011
Lars Hanisch <dvb@flensrocker.de>
2013-01-03 00:55:21 +0100

    ignore case on hostname comparison

--- avahi-wakeup.py
+++ avahi-wakeup.py
@@ -23,16 +23,16 @@ def get_broadcast_addr(interface, protocol):
 
 def wake_on_lan(macaddress, broadcast):
   if len(macaddress) == 12:
-      pass
+    pass
   elif len(macaddress) == 12 + 5:
-      sep = macaddress[2]
-      macaddress = macaddress.replace(sep, '')
+    sep = macaddress[2]
+    macaddress = macaddress.replace(sep, '')
   else:
-      raise ValueError('Incorrect MAC address format')
+    raise ValueError('Incorrect MAC address format')
   data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
   send_data = '' 
   for i in range(0, len(data), 2):
-      send_data = ''.join([send_data, struct.pack('B', int(data[i: i + 2], 16))])
+    send_data = ''.join([send_data, struct.pack('B', int(data[i: i + 2], 16))])
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
   sock.sendto(send_data, (broadcast, 7))
@@ -45,18 +45,19 @@ class dbusHostService(dbus.service.Object):
 
   @dbus.service.method(dbus_interface, in_signature = 's', out_signature = 'b')
   def WakeupHost(self, remote):
-    if remote not in hosts:
+    lowerRemote = remote.lower()
+    if lowerRemote not in hosts:
       return False
     broadcast = get_broadcast_addr(interface, netifaces.AF_INET)
     if not broadcast:
       return False
-    print "wake up " + remote + " with MAC " + hosts[remote] + " on broadcast address " + broadcast
-    wake_on_lan(hosts[remote], broadcast)
+    print "wake up " + remote + " with MAC " + hosts[lowerRemote] + " on broadcast address " + broadcast
+    wake_on_lan(hosts[lowerRemote], broadcast)
     return True
 
 
 if __name__ == '__main__':
-  hostname = socket.gethostname()
+  hostname = socket.gethostname().lower()
   hosts[hostname] = get_mac(interface)
   hostService = dbusHostService(dbus.SystemBus())
   print 'host ' + hostname + ' has MAC ' + hosts[hostname]
