c6b6f0e009cc11678e3ccdb52c68dc6080230653
Lars Hanisch <dvb@flensrocker.de>
2013-01-02 23:41:45 +0100

    add get_broadcast_addr

--- avahi-wakeup.py
+++ avahi-wakeup.py
@@ -5,3 +5,8 @@ import netifaces
 def get_mac(interface):
   return netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
 
+def get_broadcast_addr(interface, protocol):
+  return netifaces.ifaddresses(interface)[protocol][0]['broadcast']
+
+# get_mac('eth0')
+# get_broadcast_addr('eth0', netifaces.AF_INET)
