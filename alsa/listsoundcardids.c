15fc2a84f13e43f72bdecc150fee31bfd0232443
Lars Hanisch <dvb@flensrocker.de>
2013-12-29 14:06:43 +0100

    listsoundcardids: return ALSA addresses

--- alsa/listsoundcardids.c
+++ alsa/listsoundcardids.c
@@ -7,11 +7,11 @@
  * listsoundcardids
  *
  * Outputs something like:
- * Intel:VT1708S Analog
- * Intel:VT1708S Digital
- * Intel:VT1708S HP
- * NVidia:HDMI 0
- * NVidia:HDMI 1
+ * Intel (VT1708S Analog): hw:CARD=Intel,DEV=0
+ * Intel (VT1708S Digital): hw:CARD=Intel,DEV=1
+ * Intel (VT1708S HP): hw:CARD=Intel,DEV=2
+ * NVidia (HDMI 0): hw:CARD=NVidia,DEV=3
+ * NVidia (HDMI 1): hw:CARD=NVidia,DEV=7
  */
 
 static int count_strings(char **Array)
@@ -51,11 +51,14 @@ static void add_string(char ***Array, const char *String)
   (*Array)[count + 1] = NULL;
 }
 
-static int list_soundcard_ids(char ***CardId, char ***DeviceId)
+static int list_soundcard_ids(char ***CardId, char ***DeviceId, char ***AlsaAddress)
 {
   int count = 0;
-  char       hwname[32];
+  char       hw_name[16];
   snd_ctl_t *handle = NULL;
+  char      *alsa_address = NULL;
+  int        alsa_len = 0;
+  int        alsa_tmp = 0;
 
   int                  card_index = -1;
   const char          *card_id = NULL;
@@ -67,6 +70,7 @@ static int list_soundcard_ids(char ***CardId, char ***DeviceId)
 
   *CardId = NULL;
   *DeviceId = NULL;
+  *AlsaAddress = NULL;
 
   if (snd_ctl_card_info_malloc(&card_info) < 0) {
      count = -1;
@@ -81,8 +85,8 @@ static int list_soundcard_ids(char ***CardId, char ***DeviceId)
        if ((snd_card_next(&card_index) < 0) || (card_index < 0))
           break;
 
-       sprintf(hwname, "hw:%d", card_index);
-       if (snd_ctl_open(&handle, hwname, 0) == 0) {
+       snprintf(hw_name, sizeof(hw_name), "hw:%d", card_index);
+       if (snd_ctl_open(&handle, hw_name, 0) == 0) {
           if (snd_ctl_card_info(handle, card_info) == 0) {
              card_id = snd_ctl_card_info_get_id(card_info);
 
@@ -97,8 +101,18 @@ static int list_soundcard_ids(char ***CardId, char ***DeviceId)
                   snd_pcm_info_set_stream(pcm_info, SND_PCM_STREAM_PLAYBACK);
                   if (snd_ctl_pcm_info(handle, pcm_info) == 0) {
                      device_id = snd_pcm_info_get_id(pcm_info);
+                     alsa_len = 15 + strlen(card_id); /* hw:CARD=card_id,DEV=device_index */
+                     alsa_tmp = device_index;
+                     while (alsa_tmp > 9) {
+                           alsa_len++;
+                           alsa_tmp = alsa_tmp / 10;
+                           }
+                     alsa_address = malloc(alsa_len + sizeof(char));
+                     snprintf(alsa_address, alsa_len, "hw:CARD=%s,DEV=%d", card_id, device_index);
                      add_string(CardId, card_id);
                      add_string(DeviceId, device_id);
+                     add_string(AlsaAddress, alsa_address);
+                     free(alsa_address);
                      count++;
                      }
                 } while (1);
@@ -120,14 +134,16 @@ int main(int argc, char *argv[])
 {
   char **CardId = NULL;
   char **DeviceId = NULL;
+  char **AlsaAddress = NULL;
   int count = 0;
   int i = 0;
   
-  count = list_soundcard_ids(&CardId, &DeviceId);
+  count = list_soundcard_ids(&CardId, &DeviceId, &AlsaAddress);
   for (i = 0; i < count; i++)
-      printf("%s:%s\n", CardId[i], DeviceId[i]);
+      printf("%s (%s): %s\n", CardId[i], DeviceId[i], AlsaAddress[i]);
 
   free_array(CardId);
   free_array(DeviceId);
+  free_array(AlsaAddress);
   return 0;
 }
